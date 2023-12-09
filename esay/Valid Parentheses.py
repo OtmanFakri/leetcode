754d129ebe09b9a4286b6e4fc7ed82b8efdf7aa1
otmanfakri <otmanhero00@gmail.com>
2023-12-09 07:16:17 -0500

    2

---
+++ esay/Valid Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        opening_brackets = {'(', '[', '{'}
+        closing_brackets = {')': '(', ']': '[', '}': '{'}
+
+        for bracket in s:
+            if bracket in opening_brackets:
+                stack.append(bracket)
+            elif bracket in closing_brackets:
+                if not stack or stack.pop() != closing_brackets[bracket]:
+                    return False
+
+        return not stack
+
+
+if __name__ == "__main__":
+        solution = Solution()
+        s = "[]"
+        print(solution.isValid(s))
